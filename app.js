7a0cb467775807b2669d0887af53c9f29ebb4598
Matija <cresnik.matija@gmail.com>
2022-11-02 12:54:51 +0100

    Create the number of squares per user request

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const container = document.querySelector('.container')
-
+const button = document.querySelector('button')
+const newContainer = document.querySelector('.container2')
 
 
 
 for (let i = 1; i <= 256; i++){
-const divs = document.createElement('div') 
-divs.style.cssText = 'background-color:orange; width:20px;height:20px;margin:2px;'
-container.appendChild(divs)
+    const div = document.createElement('div') 
+    div.style.cssText = 'background-color:black;width:20px;height:20px;'
+    container.appendChild(div)
 
 }
 
-divs.addEventListener('mouseover', () => {
-    divs.style.cssText = 'background-color:black'
+
+container.addEventListener('mouseover', function(e) {
+const target = e.target
+
+ if(target.matches('div')){
+     target.style.backgroundColor = 'white'
+ }
 })
+
+button.addEventListener('click', () => {
+    const userInput = prompt('How many squares per side do you want your grid to be?')
+    if (prompt) {
+        container.remove()
+        const userNumber = parseInt(userInput)*parseInt(userInput)
+        for (let i = 1; i <= userNumber; i++){
+            const div = document.createElement('div') 
+            div.style.cssText = 'background-color:black;width:20px;height:20px;'
+            newContainer.appendChild(div)  
+    }    
+
+}
+})
+
+newContainer.addEventListener('mouseover', function(e) {
+    const target = e.target
+    
+     if(target.matches('div')){
+         target.style.backgroundColor = 'white'
+     }
+    })
