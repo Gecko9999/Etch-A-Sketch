dc3964e40e61f3e34de84641d5b668d2fddca0ad
Matija <cresnik.matija@gmail.com>
2022-11-03 11:19:52 +0100

    Add a costum option for the number of squares per side

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const container = document.querySelector('.container')
 const button = document.querySelector('button')
-const newContainer = document.querySelector('.container2')
+ const newContainer = document.querySelector('.container2')
 
 
 
 for (let i = 1; i <= 256; i++){
     const div = document.createElement('div') 
-    div.style.cssText = 'background-color:black;width:20px;height:20px;'
+    div.style.cssText = 'background-color:black;width:20px;height:20px'
     container.appendChild(div)
 
 }
@@ -19,21 +19,34 @@ const target = e.target
      target.style.backgroundColor = 'white'
  }
 })
-
 button.addEventListener('click', () => {
-    const userInput = prompt('How many squares per side do you want your grid to be?')
-    if (prompt) {
+    
+    function getInput(){
+    const userInput = prompt('How many squares per side do you want your Etch-a-sketch to be? (max 100)')
+    if (prompt && parseInt(userInput) <= 100) {
         container.remove()
         const userNumber = parseInt(userInput)*parseInt(userInput)
         for (let i = 1; i <= userNumber; i++){
             const div = document.createElement('div') 
-            div.style.cssText = 'background-color:black;width:20px;height:20px;'
+            div.style.cssText = 'background-color:black;'
+            div.style.width = 320/parseInt(userInput)+"px"
+            div.style.height = 320/parseInt(userInput)+"px"
             newContainer.appendChild(div)  
     }    
+}
+        
+else if (parseInt(userInput) > 100){
+    getInput()
+  }
+}
+
+getInput()
+
+
+})
+{
 
 }
-})
-
 newContainer.addEventListener('mouseover', function(e) {
     const target = e.target
     
