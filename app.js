4b384251e578157a87446a57c1b04995afeb1536
Matija <cresnik.matija@gmail.com>
2022-11-06 15:26:11 +0100

    -

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const container = document.querySelector('.container')
 const button = document.querySelector('button')
 const newContainer = document.querySelector('.container2')
-const button2 = document.querySelector('.rgb')
+const button2 = document.querySelector('#rgb')
+const button3 = document.querySelector('.reset')
 
 for (let i = 1; i <= 256; i++){
     const div = document.createElement('div') 
@@ -17,6 +18,14 @@ const target = e.target
  }
 })
 
+
+newContainer.addEventListener('mouseover', function(e) {
+    const target = e.target
+     if(target.matches('.diva')){
+         target.style.backgroundColor = 'black'
+        }
+    })
+    
 button.addEventListener('click', () => {
     function getInput(){
     const userInput = prompt('How many squares per side do you want your Etch-a-sketch to be? (max 100)')
@@ -40,17 +49,37 @@ else {
 getInput()
 })
 
+document.getElementById('#rgb').onclick = function() {
+    newContainer.addEventListener('mouseover', function(e) {
+        const target = e.target
+         if(target.matches('.diva')){
+         function getRandomRGB(){
+           const x = Math.floor(Math.random()*256)
+           const y = Math.floor(Math.random()*256)
+           const z = Math.floor(Math.random()*256)
+           const RGBColor = "rgb("+x+","+y+","+z+")";    
+           return RGBColor
+       }
+          return  target.style.backgroundColor = getRandomRGB()
+         }
+        })
+}
+
+   
+
+/*
 newContainer.addEventListener('mouseover', function(e) {
     const target = e.target
-    
      if(target.matches('.diva')){
-        function getRandomRGB(){
-            const x = Math.floor(Math.random()*256)
-            const y = Math.floor(Math.random()*256)
-            const z = Math.floor(Math.random()*256)
-            const RGBColor = "rgb("+x+","+y+","+z+")";
-            return RGBColor
-        }
+     function getRandomRGB(){
+       const x = Math.floor(Math.random()*256)
+       const y = Math.floor(Math.random()*256)
+       const z = Math.floor(Math.random()*256)
+       const RGBColor = "rgb("+x+","+y+","+z+")";    
+       return RGBColor
+   }
         target.style.backgroundColor = getRandomRGB()
      }
     })
+
+    */
